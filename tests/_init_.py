c7e0fcb4f759ab121e22b7a23cad61812eaeb313
adergu <amaredergu2121@gmail.com>
2024-11-28 22:00:16 +0100

    init

---
+++ tests/_init_.py
@@ -0,0 +1,7 @@
+# tests/__init__.py
+
+import sys
+from os.path import abspath, dirname
+
+# Add the src directory to the system path for tests
+sys.path.insert(0, abspath(dirname(dirname(__file__))) + "/src")
