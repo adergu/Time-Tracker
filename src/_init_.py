935b080a2fd5918a3026ccbd0abf0d27670d1067
Amare Dergu Mengstie <90146846+adergu@users.noreply.github.com>
2024-11-28 23:09:01 +0100

    Renaming _init

--- src/_init_.py
+++
@@ -1,4 +0,0 @@
-# src/__init__.py
-from .time_tracker import *  # Import everything from time_tracker
-from .analytics import *     # Import everything from analytics
-from .utils import *         # Import everything from utils
