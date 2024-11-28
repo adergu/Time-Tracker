5986f269f4eafdef03f38e4a70042f2235ed8781
Amare Dergu Mengstie <90146846+adergu@users.noreply.github.com>
2024-11-28 23:16:17 +0100

    _init_ for src directory

--- src/_init.py
+++
@@ -1,4 +0,0 @@
-# src/__init.py
-from .time_tracker import *  # Import everything from time_tracker
-from .analytics import *     # Import everything from analytics
-from .utils import *         # Import everything from utils
